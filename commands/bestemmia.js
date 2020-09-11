53979d49089b1ae69e57b81da71e3d6cdbcf6dfa
Tommix07 <49081257+Tommix07@users.noreply.github.com>
2020-09-11 23:25:38 +0200

    Update bestemmia.js

--- commands/bestemmia.js
+++ commands/bestemmia.js
@@ -2,9 +2,9 @@ module.exports = {
     name: 'bestemmia',
     description: "LOLOLOL",
     execute(message, args) {
-        message.channel.send('Porco dio e porca la madonna'
-                             'tutti gli angeli in colonna'
-                             'gli ho contati son 31'
-                             'Porco Dio ne manca 1!')
+        message.channel.send('Porco dio e porca la madonna')
+        message.channel.send('tutti gli angeli in colonna')
+        message.channel.send('gli ho contati son 31')
+        message.channel.send('Porco Dio ne manca 1!')
     }
 }
