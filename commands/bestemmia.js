945b2ad9762581c7479e372cf259b22468163b2d
Tommix07 <49081257+Tommix07@users.noreply.github.com>
2020-09-11 23:19:21 +0200

    Create bestemmia.js

---
+++ commands/bestemmia.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'bestemmia',
+    description: "LOLOLOL",
+    execute(message, args) {
+        message.channel.send('Porco dio e porca la madonna
+        tutti gli angeli in colonna
+        gli ho contati son 31
+        Porco Dio ne manca 1!')
+    }
+}
