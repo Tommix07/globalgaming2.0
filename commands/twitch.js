769312acaac7c10fe59b75b30dc44621660ba994
Tommix07 <49081257+Tommix07@users.noreply.github.com>
2020-09-12 21:23:20 +0200

    Add files via upload

---
+++ commands/twitch.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'twitch',
+    description: "Twitch command",
+    execute(message, args){
+        message.channel.send('https://www.twitch.tv/globalgamingmi')
+    }
+}
