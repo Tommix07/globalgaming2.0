d6942de15ef8509e1db8fbe42dd019e378fdd405
Tommix07 <49081257+Tommix07@users.noreply.github.com>
2020-09-12 15:35:01 +0200

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,37 @@
+﻿module.exports = {
+    name: 'help',
+    description: "Help",
+    execute(message, args) {
+    const Discord = require('discord.js');
+        if (!args.length) {
+            const helpEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Comandi per Global Gaming 2.0 (bot):')
+                .addFields(
+                    { name: 'Moderazione', value: '‭‭gg!help mod' },
+                    { name: 'gg!site', value: 'Sito web ufficiale di Global Gaming 2.0', inline: true },
+                    { name: 'gg!rules', value: 'Regolamento', inline: true },
+                    { name: 'gg!filastrocca', value: 'Provalo...', inline: true },
+                )
+                .setFooter(message.author.username);
+
+
+            message.reply(helpEmbed);
+
+        } else if (args[0] === 'mod') {
+
+            const helpModEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Comandi per Global Gaming 2.0 (bot):')
+                .addFields(
+                    { name: 'Moderazione', value: '‭‭ ' },
+                    { name: 'gg!ban', value: 'Per bannare un utente dal server', inline: true },
+                    { name: 'gg!kick', value: 'Per chiccare un utente dal server', inline: true },
+                )
+                .setFooter(message.author.username);
+
+
+            message.reply(helpModEmbed);
+        }
+    }
+}
