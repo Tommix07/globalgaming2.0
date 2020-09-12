306a9918085f6932c5044fc357e2321186a0512e
Tommix07 <49081257+Tommix07@users.noreply.github.com>
2020-09-12 15:37:48 +0200

    Update site.js

--- commands/site.js
+++ commands/site.js
@@ -2,6 +2,17 @@ module.exports = {
     name: 'site',
     description: "Sito",
     execute(message, args) {
-        message.channel.send('Il sito web di global gaming sar\u00E0 presto disponibile!')
+        const Discord = require('discord.js');
+        const siteEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Il sito web ufficiale di Global Gaming 2.0:')
+            .addFields(
+                { name: 'http://globalgaming.altervista.org/', value: '‭‭‭‭  ' },
+            )
+            .setFooter(message.author.username);
+
+
+        message.reply(siteEmbed);
+
     }
 }
